6662e297cc4dba5a6a76cf041e5b20d46e0cfafc
Ivo de Kler <ivo.de.kler@gmail.com>
2020-12-25 11:14:26 +0100

    set to port 80

--- accordeon.js
+++ accordeon.js
@@ -28,7 +28,7 @@ var oneYear = 31557600000,
     };
 
 app.configure(function(){
-  app.set('port', 8052);
+  app.set('port', 80);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.static(path.join(__dirname, 'public'), { maxAge: clientCacheLimit } ));
